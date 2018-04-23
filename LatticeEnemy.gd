013fa6fe2ff77d877776c90f94635336ab21a879
Nate Sampo <nathan.sampo@students.olin.edu>
2018-04-23 00:31:10 -0400

    laticestuff

--- LatticeEnemy.gd
+++ LatticeEnemy.gd
@@ -12,21 +12,31 @@ var linkWidth = 5;
 var player
 
 func _process(delta):
+	for child in get_node("/root").find_node("Enemies").get_children():
+		if child.
+	
+	for child in get_parent().find_node("Linkage").get_children():
+		if (child != get_parent().find_node("Linkage").find_node("Armor") and child != get_parent().find_node("Linkage").find_node("Sprite")):
+			child.free();
+	
 	for link in links:
 		var shape = ConvexPolygonShape2D.new();
 		var shapePoints = PoolVector2Array();
+		var collision = CollisionShape2D.new();
 		shapePoints.append(global_position - Vector2(linkWidth/2, 0));
 		shapePoints.append(global_position + Vector2(linkWidth/2, 0));
 		shapePoints.append(link[0].global_position + Vector2(linkWidth/2, 0));
 		shapePoints.append(link[0].global_position - Vector2(linkWidth/2, 0));
 		shape.points = shapePoints;
-		#link[1].free();
+		collision.set_shape(shape);
+		get_parent().find_node("Linkage").add_child(collision);
 		link[1] = shape;
 	update();
 
-func _ready():
-	connect("body_entered", self, "_check_for_links");
-	connect("body_exited", self, "_remove_links");
+#func _ready():
+#	connect("body_entered", self, "_check_for_links");
+#	connect("body_exited", self, "_remove_links");
+
 
 func _check_for_links(body):
 	if (body.is_in_group("link") and body.get_parent() != get_parent()):
@@ -39,13 +49,13 @@ func _check_for_links(body):
 		shapePoints.append(body.global_position - Vector2(linkWidth/2, 0));
 		shape.points = shapePoints;
 		collision.set_shape(shape);
-		
+		get_parent().find_node("Linkage").add_child(collision);
 		links.push_back([body, collision]);
 		
 func _remove_links(body):
 	if body.is_in_group("link"):
 		for i in range(links.size() - 1):
-			if links[i] == body:
+			if links[i][0] == body:
 				links.remove(i);
 				break;
 		
